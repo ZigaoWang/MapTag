d2dd94cfcccfe21b8bd9f1a008a6b3dd0afa5732
Zigao Wang <zigao.wang24@gmail.com>
2025-07-07 22:35:28 +0800

    feat: empty ios project

---
+++ MapTag/MapTag/MapTagApp.swift
@@ -0,0 +1,17 @@
+//
+//  MapTagApp.swift
+//  MapTag
+//
+//  Created by Zigao Wang on 7/7/25.
+//
+
+import SwiftUI
+
+@main
+struct MapTagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
